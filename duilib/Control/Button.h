b122acf0784870d215e3fdfd95ca701cfaa78aa2
redrain <redrains@qq.com>
2021-02-26 15:03:51 +0800

    拆分Button、Label、Option控件 fix：Option切换分组名称的逻辑问题

--- duilib/Control/Button.h
+++ duilib/Control/Button.h
@@ -24,52 +24,7 @@ public:
 	void AttachClick(const EventCallback& callback)	{ this->OnEvent[kEventClick] += callback;	}
 };
 
-template<typename InheritType>
-UINT ui::ButtonTemplate<InheritType>::GetControlFlags() const
-{
-	return this->IsKeyboardEnabled() && this->IsEnabled() ? UIFLAG_TABSTOP : UIFLAG_DEFAULT;
-}
-
-template<typename InheritType>
-ButtonTemplate<InheritType>::ButtonTemplate()
-{
-	this->m_uTextStyle = DT_VCENTER | DT_CENTER | DT_END_ELLIPSIS | DT_NOCLIP | DT_SINGLELINE;
-}
-
-template<typename InheritType>
-void ButtonTemplate<InheritType>::HandleMessage(EventArgs& event)
-{
-	if (!this->IsMouseEnabled() && event.Type > kEventMouseBegin && event.Type < kEventMouseEnd) {
-		if (this->m_pParent != NULL) this->m_pParent->HandleMessageTemplate(event);
-		else __super::HandleMessage(event);
-		return;
-	}
-	if (event.Type == kEventKeyDown)
-	{
-		if (this->IsKeyboardEnabled()) {
-			if (event.chKey == VK_SPACE || event.chKey == VK_RETURN) {
-				Activate();
-				return;
-			}
-		}
-	}
-	if (event.Type == kEventInternalMenu)
-	{
-		if (this->IsContextMenuUsed()) {
-			this->m_pWindow->SendNotify(this, kEventMouseMenu, event.wParam, event.lParam);
-		}
-		return;
-	}
-
-	__super::HandleMessage(event);
-}
-
-template<typename InheritType>
-void ButtonTemplate<InheritType>::Activate()
-{
-	if (!this->IsActivatable()) return;
-	if (this->m_pWindow != NULL) this->m_pWindow->SendNotify(this, kEventClick);
-}
+#include "ButtonImpl.h"
 
 typedef ButtonTemplate<Control> Button;
 typedef ButtonTemplate<Box> ButtonBox;
