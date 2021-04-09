df02ecd2fe333ff73cd1b965707dcd3153372cd2
sylar <sylar.huang@all-dream.com>
2021-04-09 23:40:58 +0800

    add marquee progress

--- duilib/Control/Progress.cpp
+++ duilib/Control/Progress.cpp
@@ -12,7 +12,12 @@ Progress::Progress() :
 	m_nValue(0),
 	m_sProgressColor(),
 	m_progressImage(),
-	m_sProgressImageModify()
+	m_sProgressImageModify(),
+	m_bMarquee(false),
+	m_nMarqueeWidth(10),
+	m_nMarqueeStep(4),
+	m_nMarqueeElapsed(50), // for 1s 25fps,will use 20fps default
+	m_nMarqueePos(0)
 {
 	m_uTextStyle = DT_SINGLELINE | DT_CENTER;
 	SetFixedHeight(12);
@@ -41,7 +46,7 @@ bool Progress::IsHorizontal()
 
 void Progress::SetHorizontal(bool bHorizontal)
 {
-	if( m_bHorizontal == bHorizontal ) return;
+	if (m_bHorizontal == bHorizontal) return;
 
 	m_bHorizontal = bHorizontal;
 	Invalidate();
@@ -123,7 +128,7 @@ std::wstring Progress::GetProgressColor() const
 void Progress::SetProgressColor(const std::wstring& strProgressColor)
 {
 	ASSERT(this->GetWindowColor(strProgressColor) != 0);
-	if( m_sProgressColor == strProgressColor ) return;
+	if (m_sProgressColor == strProgressColor) return;
 
 	m_sProgressColor = strProgressColor;
 	Invalidate();
@@ -131,32 +136,40 @@ void Progress::SetProgressColor(const std::wstring& strProgressColor)
 
 void Progress::SetAttribute(const std::wstring& srName, const std::wstring& strValue)
 {
-	if( srName == _T("hor") ) SetHorizontal(strValue == _T("true"));
-	else if( srName == _T("min") ) SetMinValue(_ttoi(strValue.c_str()));
-	else if( srName == _T("max") ) SetMaxValue(_ttoi(strValue.c_str()));
+	if (srName == _T("hor")) SetHorizontal(strValue == _T("true"));
+	else if (srName == _T("min")) SetMinValue(_ttoi(strValue.c_str()));
+	else if (srName == _T("max")) SetMaxValue(_ttoi(strValue.c_str()));
 	else if (srName == _T("value")) SetValue(_ttoi(strValue.c_str()));
 	else if (srName == _T("progressimage")) SetProgressImage(strValue);
 	else if (srName == _T("isstretchfore")) SetStretchForeImage(strValue == _T("true"));
-	else if( srName == _T("progresscolor") ) {
+	else if (srName == _T("progresscolor")) {
 		LPCTSTR pValue = strValue.c_str();
-		while( *pValue > _T('\0') && *pValue <= _T(' ') ) pValue = ::CharNext(pValue);
+		while (*pValue > _T('\0') && *pValue <= _T(' ')) pValue = ::CharNext(pValue);
 		SetProgressColor(pValue);
 	}
+	else if (srName == _T("marquee")) SetMarquee(strValue == _T("true"));
+	else if (srName == _T("marqueewidth")) SetMarqueeWidth(_ttoi(strValue.c_str()));
+	else if (srName == _T("marqueestep")) SetMarqueeStep(_ttoi(strValue.c_str()));
 	else Label::SetAttribute(srName, strValue);
 }
 
 void Progress::PaintStatusImage(IRenderContext* pRender)
 {
-	if( m_nMax <= m_nMin ) m_nMax = m_nMin + 1;
-	if( m_nValue > m_nMax ) m_nValue = m_nMax;
-	if( m_nValue < m_nMin ) m_nValue = m_nMin;
+	if (m_bMarquee) {
+		PaintMarquee(pRender);
+		return;
+	}
+
+	if (m_nMax <= m_nMin) m_nMax = m_nMin + 1;
+	if (m_nValue > m_nMax) m_nValue = m_nMax;
+	if (m_nValue < m_nMin) m_nValue = m_nMin;
 
 	UiRect rc = GetProgressPos();
 	if (!m_sProgressColor.empty()) {
 		DWORD dwProgressColor = this->GetWindowColor(m_sProgressColor);
-		if( dwProgressColor != 0 ) {
+		if (dwProgressColor != 0) {
 			UiRect rcProgressColor = m_rcItem;
-			if( m_bHorizontal ) {
+			if (m_bHorizontal) {
 				rcProgressColor.right = rcProgressColor.left + rc.right;
 			}
 			else {
@@ -172,12 +185,12 @@ void Progress::PaintStatusImage(IRenderContext* pRender)
 			m_sProgressImageModify = StringHelper::Printf(_T("destscale='false' dest='%d,%d,%d,%d'"), rc.left, rc.top, rc.right, rc.bottom);
 		else
 			m_sProgressImageModify = StringHelper::Printf(_T("destscale='false' dest='%d,%d,%d,%d' source='%d,%d,%d,%d'")
-			, rc.left, rc.top, rc.right, rc.bottom
-			, rc.left, rc.top, rc.right, rc.bottom);
+				, rc.left, rc.top, rc.right, rc.bottom
+				, rc.left, rc.top, rc.right, rc.bottom);
 
 		// 让corner的值不超过可绘制范围
 		auto& corner = m_progressImage.imageAttribute.rcCorner;
-		if (IsHorizontal())	{
+		if (IsHorizontal()) {
 			if (corner.left != 0 && corner.left >= rc.right) {
 				m_sProgressImageModify += StringHelper::Printf(_T(" corner='%d,%d,%d,%d'"),
 					rc.right,
@@ -211,7 +224,7 @@ UiRect Progress::GetProgressPos()
 		rc.right = m_rcItem.right - m_rcItem.left;
 		rc.bottom = m_rcItem.bottom - m_rcItem.top;
 	}
-		
+
 	return rc;
 }
 
@@ -219,6 +232,119 @@ void Progress::ClearImageCache()
 {
 	__super::ClearImageCache();
 	m_progressImage.ClearCache();
+}
+
+void Progress::Play() {
+	if (!m_bMarquee) {
+		m_timer.Cancel();
+		return;
+	}
+	m_nMarqueePos = m_nMarqueePos + m_nMarqueeStep;
+
+	ui::UiRect rc = m_rcItem;
+	if (m_bHorizontal) {
+		if (m_nMarqueePos > rc.right - rc.left)
+			m_nMarqueePos = (m_nMarqueePos - (rc.right - rc.left)) - m_nMarqueeWidth;
+	}
+	else {
+		if (m_nMarqueePos > rc.bottom - rc.top)
+			m_nMarqueePos = (m_nMarqueePos - (rc.bottom - rc.top) - m_nMarqueeWidth);
+	}
+
+	Invalidate();
+}
+
+void Progress::PaintMarquee(IRenderContext* pRender) {
+	if (!m_sProgressColor.empty()) {
+		DWORD dwProgressColor = GlobalManager::GetTextColor(m_sProgressColor);
+		if (dwProgressColor != 0) {
+			UiRect rcProgressColor = m_rcItem;
+			ui::UiRect rc = m_rcItem;
+			if (m_bHorizontal) {
+				rc.left = max(m_nMarqueePos, 0) + rc.left;
+				rc.right = rc.left + (m_nMarqueePos >= 0 ? m_nMarqueeWidth : (m_nMarqueeWidth + m_nMarqueePos));
+			}
+			else {
+				rc.top = max(m_nMarqueePos, 0) + rc.top;
+				rc.bottom = rc.top + (m_nMarqueePos >= 0 ? m_nMarqueeWidth : (m_nMarqueeWidth + m_nMarqueePos));
+			}
+			DpiManager::GetInstance()->ScaleRect(rc);
+			pRender->DrawColor(rc, dwProgressColor);
+		}
+	}
+}
+
+bool Progress::IsMarquee()
+{
+	return m_bMarquee;
+}
+
+void Progress::SetMarquee(bool bMarquee)
+{
+	if (m_bMarquee == bMarquee)
+		return;
+
+	m_bMarquee = bMarquee;
+	m_nMarqueePos = 0;
+
+	if (m_bMarquee) {
+		auto playCallback = nbase::Bind(&Progress::Play, this);
+		TimerManager::GetInstance()->AddCancelableTimer(m_timer.GetWeakFlag(), playCallback, m_nMarqueeElapsed, TimerManager::REPEAT_FOREVER);
+	}
+	else {
+		m_timer.Cancel();
+	}
+
+	Invalidate();
+}
+
+int Progress::GetMarqueeWidth()
+{
+	return m_nMarqueeWidth;
+}
+
+void Progress::SetMarqueeWidth(int nMarqueeWidth)
+{
+	if (m_nMarqueeWidth == nMarqueeWidth)
+		return;
+
+	m_nMarqueeWidth = nMarqueeWidth;
+
+	Invalidate();
+}
+
+int Progress::GetMarqueeStep()
+{
+	return m_nMarqueeStep;
+}
+
+void Progress::SetMarqueeStep(int nMarqueeStep)
+{
+	if (m_nMarqueeStep == nMarqueeStep)
+		return;
+
+	m_nMarqueeStep = nMarqueeStep;
+
+	Invalidate();
+}
+
+int Progress::GetMarqueeElapsed()
+{
+	return m_nMarqueeElapsed;
+}
+
+void Progress::SetMarqueeElapsed(int nMarqueeElapsed)
+{
+	if (!IsMarquee() || m_nMarqueeElapsed == m_nMarqueeElapsed)
+		return;
+
+	m_nMarqueeElapsed = nMarqueeElapsed;
+	m_timer.Cancel();
+
+	auto playCallback = nbase::Bind(&Progress::Play, this);
+	TimerManager::GetInstance()->AddCancelableTimer(m_timer.GetWeakFlag(), playCallback, m_nMarqueeElapsed, TimerManager::REPEAT_FOREVER);
+
+	Invalidate();
 }
 
 }
